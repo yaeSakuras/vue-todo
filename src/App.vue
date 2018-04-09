e22c630fcfcfe6cae67d475cc8b9826132ff321f
zhouzh <363420113@qq.com>
2018-04-10 00:04:23 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">{{ text }}</div>
+</template>
+<script>
+export default {
+    data() {
+       return {
+           text:'abc'
+       }     
+    }
+}
+</script>
+<style>
+#app{
+    color: red;
+}
+</style>
+
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+
+module.exports = {
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename:'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.styl/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024,
+                            name:'[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}

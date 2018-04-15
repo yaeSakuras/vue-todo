268d9975568fea2833fc70d6cafb8cced3c1a29c
zhouzh <363420113@qq.com>
2018-04-16 01:17:36 +0800

    优化webpack打包

--- src/App.vue
+++ src/App.vue
@@ -19,22 +19,22 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-    #app{
-        position absolute
-        left 0
-        right 0
-        top 0
-        bottom 0
-    }
-    #cover{
-        position absolute
-        left 0
-        top 0
-        right 0
-        bottom 0
-        background-color #999
-        opacity .9
-        z-index -1
-    }
+#app{
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+}
+#cover{
+    position absolute
+    left 0
+    top 0
+    right 0
+    bottom 0
+    background-color #999
+    opacity .9
+    z-index -1
+}
 </style>
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const webpack = require('webpack')
 
 const isDev = process.env.NODE_ENV === 'development'
@@ -8,7 +9,7 @@ const config = {
     target: 'web',
     entry: path.join(__dirname, 'src/index.js'),
     output: {
-        filename:'bundle.js',
+        filename:'bundle.[hash:8].js',
         path: path.join(__dirname, 'dist')
     },
     module: {
@@ -20,27 +21,6 @@ const config = {
             {
                 test: /\.jsx$/,
                 loader: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.styl/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    'stylus-loader'
-                ]
             },
             {
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
@@ -67,6 +47,20 @@ const config = {
 }
 
 if(isDev){
+    config.module.rules.push({
+        test: /\.styl/,
+        use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            'stylus-loader'
+        ]
+    })
     config.devtool = '#cheap-module-eval-source-map'
     config.devServer = {
         port: 8000,
@@ -80,6 +74,38 @@ if(isDev){
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
+    )
+}else{
+    config.entry = {
+        app: path.join(__dirname, 'src/index.js'),
+        vendor:['vue']
+    }
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push({
+        test: /\.styl/,
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                'stylus-loader'
+            ]
+        })
+    })
+    config.plugins.push(
+        new ExtractTextPlugin('styles.[contentHash:8].css'),
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'vendor'
+        }),
+        //webpack相关代码打包到一个文件
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'runtime'
+        })
     )
 }
 
